3b89b47b81a97646464c0e419dbebd2d968e7bf6
Oleg <54029296+InferKing@users.noreply.github.com>
2024-11-01 23:58:07 +0500

    Another init commit

---
+++ Assets/_Project/Scripts/EventBus/CallbackWithPriority.cs
@@ -0,0 +1,10 @@
+public class CallbackWithPriority
+{
+    public readonly int Priority;
+    public readonly object Callback;
+    public CallbackWithPriority(int priority, object callback)
+    {
+        Priority = priority;
+        Callback = callback;
+    }
+}
