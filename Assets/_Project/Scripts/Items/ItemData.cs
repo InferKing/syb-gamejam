eb9113bf8bedbff397e6118dc735b80ea6e7e4e4
Oleg <54029296+InferKing@users.noreply.github.com>
2024-11-02 01:11:55 +0500

    fix whitespaces in ItemData.cs

--- Assets/_Project/Scripts/Items/ItemData.cs
+++ Assets/_Project/Scripts/Items/ItemData.cs
@@ -5,9 +5,9 @@ namespace Model.Items
 	[CreateAssetMenu(fileName = "Cfg_Item_", menuName = "Item")]
 	public class ItemData : ScriptableObject
 	{
-        [field: SerializeField]
-        public ItemKey ItemKey { get; private set; }
-        [field: SerializeField]
+		[field: SerializeField]
+		public ItemKey ItemKey { get; private set; }
+		[field: SerializeField]
 		public string Name { get; private set; }
 	} 
 }
