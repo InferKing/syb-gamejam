e39d1425382f4b3687a18dfb06b0b5bc2f540696
Oleg <54029296+InferKing@users.noreply.github.com>
2024-11-02 00:54:32 +0500

    add item scripts and SO's

---
+++ Assets/_Project/Scripts/Items/ItemData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Model.Items
+{
+	[CreateAssetMenu(fileName = "Cfg_Item_", menuName = "Item")]
+	public class ItemData : ScriptableObject
+	{
+        [field: SerializeField]
+        public ItemKey ItemKey { get; private set; }
+        [field: SerializeField]
+		public string Name { get; private set; }
+	} 
+}
