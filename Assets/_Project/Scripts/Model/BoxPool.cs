7bce8e972ea05729662e05ef9ccf4061dc08f61f
Oleg <54029296+InferKing@users.noreply.github.com>
2024-11-02 15:26:40 +0500

    add boxpool

--- Assets/_Project/Scripts/Model/BoxPool.cs
+++ Assets/_Project/Scripts/Model/BoxPool.cs
@@ -1,8 +1,48 @@
-using System.Collections;
+using Model.Items;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxPool : MonoBehaviour
 {
-    
+    //TODO: ƒобавить сброс положени€, rotation и анимации коробок к стандратному состо€нию
+    [SerializeField]
+    private List<ItemData> _items;
+    [SerializeField]
+    private List<Box> _boxes;
+
+    private EventBus _bus;
+
+    private void Start()
+    {
+        _bus = ServiceLocator.Instance.Get<EventBus>();
+        _bus.Subscribe<NewTaskSignal>(OnNewTask);
+        _bus.Subscribe<PlayerEnterTerminal>(OnPlayerEnterTerminal);
+    }
+
+    private void OnNewTask(NewTaskSignal signal)
+    {
+        List<int> randomIndexes = new();
+        var items = signal.task.task.Items;
+
+        for (int i = 0; i < items.Count; i++) 
+        {
+            int randomIndex;
+            do
+            {
+                randomIndex = Random.Range(0, _items.Count);
+            }
+            while (randomIndexes.Contains(randomIndex));
+            randomIndexes.Add(randomIndex);
+        }
+
+        foreach (var idnex in randomIndexes)
+        {
+            //_boxes[index]
+        }
+    }
+
+    private void OnPlayerEnterTerminal(PlayerEnterTerminal terminal)
+    {
+        // reset boxes
+    }
 }
