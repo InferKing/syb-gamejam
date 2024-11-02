92c2aebc8558b6ef9e398dda872710a07dd211dd
Aleksandr Trushlyakov <118696262+trooon3@users.noreply.github.com>
2024-11-03 01:46:30 +0300

    ConnectSave

---
+++ Assets/_Project/Scripts/Sound/AudioController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    
+    void Start()
+    {
+        
+    }
+
+    
+}
