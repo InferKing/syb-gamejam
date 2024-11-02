bd7d33b571f232f51528791678a1415a8bfbdef0
Oleg <54029296+InferKing@users.noreply.github.com>
2024-11-02 12:53:51 +0500

    add TerminalUI

--- Assets/_Project/Scripts/Items/SceneItems/Terminal/TerminalTab.cs
+++ Assets/_Project/Scripts/Items/SceneItems/Terminal/TerminalTab.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TerminalTab : MonoBehaviour
 {
-    
+    [SerializeField]
+    private GameObject _view;
+    [SerializeField]
+    private Image _imageTab;
+
+    public bool Enabled { get; private set; } = false;
+
+    public void SetActiveTab(bool isActive)
+    {
+        Enabled = isActive;
+
+        _view.SetActive(isActive);
+        _imageTab.color = isActive ? Color.blue : Color.white;
+    }
 }
--- Assets/_Project/Scripts/Items/SceneItems/Terminal/TerminalUI.cs
+++ Assets/_Project/Scripts/Items/SceneItems/Terminal/TerminalUI.cs
@@ -1,17 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TerminalUI : MonoBehaviour
 {
     [SerializeField]
     private GameObject _root;
+    [SerializeField]
+    private List<TerminalTab> _tabs;
+    [SerializeField]
+    private List<Button> _buttons;
+
+    private void Start()
+    {
+        _buttons[0].onClick.AddListener(() => OpenTab(0));
+        _buttons[1].onClick.AddListener(() => OpenTab(1));
+    }
 
     public void Open()
     {
         _root.SetActive(true);
+
+        OpenTab(0);
     }
 
     public void Close()
     {
         _root.SetActive(false);
+    }
+
+    private void OpenTab(int index)
+    {
+        for (int i = 0; i < _tabs.Count; i++) 
+        {
+            _tabs[i].SetActiveTab(i == index);
+        }
     }
 }
