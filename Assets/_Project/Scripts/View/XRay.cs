215b70bbc72d598f13841b7f59ca2c2841a88a62
Oleg <54029296+InferKing@users.noreply.github.com>
2024-11-04 06:08:57 +0500

    some changes

--- Assets/_Project/Scripts/View/XRay.cs
+++ Assets/_Project/Scripts/View/XRay.cs
@@ -4,27 +4,32 @@ using UnityEngine;
 
 public class XRay : MonoBehaviour
 {
+    [SerializeField]
+    private Transform _playerTransform;
+
     RaycastHit oldHit;
 
     private void FixedUpdate()
     {
-        float characterDistance = Vector3.Distance(transform.position, GameObject.Find("Character").transform.position);
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        // не работает пока не добавить коллайдеры на объекты + использовать TryGetComponent
 
-        if (Physics.Raycast(transform.position, fwd, out RaycastHit hit, characterDistance))
-        {
-            if (oldHit.transform)
-            {
-                Color colorA = oldHit.transform.gameObject.GetComponent<Renderer>().material.color;
-                colorA.a = 1f;
-                oldHit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_Color", colorA);
-            }
+        //float characterDistance = Vector3.Distance(transform.position, _playerTransform.position);
+        //Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
-            Color colorB = hit.transform.gameObject.GetComponent<Renderer>().material.color;
-            colorB.a = 0.5f;
-            hit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_Color", colorB);
+        //if (Physics.Raycast(transform.position, fwd, out RaycastHit hit, characterDistance))
+        //{
+        //    if (oldHit.transform)
+        //    {
+        //        Color colorA = oldHit.transform.gameObject.GetComponent<Renderer>().material.color;
+        //        colorA.a = 1f;
+        //        oldHit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_Color", colorA);
+        //    }
 
-            oldHit = hit;
-        }
+        //    Color colorB = hit.transform.gameObject.GetComponent<Renderer>().material.color;
+        //    colorB.a = 0.5f;
+        //    hit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_Color", colorB);
+
+        //    oldHit = hit;
+        //}
     }
 }
