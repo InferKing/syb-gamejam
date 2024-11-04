d299b74c441f1bc63e40e9b043441240816b7d3e
Oleg <54029296+InferKing@users.noreply.github.com>
2024-11-04 19:05:42 +0500

    fxgh

--- Assets/_Project/Scripts/View/Podskaz.cs
+++ Assets/_Project/Scripts/View/Podskaz.cs
@@ -10,16 +10,48 @@ public class Podskaz : MonoBehaviour
 
     private EventBus _bus;
 
+    private Coroutine _coroutine;
+
     private void Start()
     {
         _bus = ServiceLocator.Instance.Get<EventBus>();
 
         _bus.Subscribe<TerminalPickedAndClosedSignal>(OnPickedTerminal);
+        _bus.Subscribe<NewTaskSignal>(OnNewTask);
+        _bus.Subscribe<GetItemInSceneSignal>(OnGetItems);
     }
 
     void OnPickedTerminal(TerminalPickedAndClosedSignal signal)
     {
-        
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+            _coroutine = null;
+        }
+        _coroutine = StartCoroutine(TypeText("Найдите все вещи, которые спрятались в коробках."));
+    }
+
+    void OnNewTask(NewTaskSignal signal)
+    {
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+            _coroutine = null;
+        }
+        StartCoroutine(TypeText("У вас появилось новое задание, подойдите к терминалу. "));
+    }
+
+    void OnGetItems(GetItemInSceneSignal signal)
+    {
+        if (ServiceLocator.Instance.Get<GameModel>().State == GameState.GetAll)
+        {
+            if (_coroutine != null)
+            {
+                StopCoroutine(_coroutine);
+                _coroutine = null;
+            }
+            StartCoroutine(TypeText("Подойдите к пневмопочте для отправки предметов герою."));
+        }
     }
 
     IEnumerator TypeText(string text)
