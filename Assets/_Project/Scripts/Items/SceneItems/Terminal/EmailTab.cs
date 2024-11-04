06100fef131925c40a71d572e7175d6639adedc8
Oleg <54029296+InferKing@users.noreply.github.com>
2024-11-04 16:39:29 +0500

    some changes

--- Assets/_Project/Scripts/Items/SceneItems/Terminal/EmailTab.cs
+++ Assets/_Project/Scripts/Items/SceneItems/Terminal/EmailTab.cs
@@ -1,5 +1,10 @@
+using UnityEngine;
+
 public class EmailTab : TerminalTab
 {
+    [SerializeField]
+    private GameObject _emailPrefab;
+
     public override void SetActiveTab(bool isActive, NewTask task)
     {
         base.SetActiveTab(isActive, task);
