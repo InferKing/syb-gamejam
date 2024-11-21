a085489e5b03611e29f199747087ec98d63eb802
Aleksandr Trushlyakov <118696262+trooon3@users.noreply.github.com>
2024-11-21 19:25:12 +0300

    FixTerminalMenuUI

--- Assets/_Project/Scripts/Items/SceneItems/Terminal/TabOrders.cs
+++ Assets/_Project/Scripts/Items/SceneItems/Terminal/TabOrders.cs
@@ -7,7 +7,9 @@ public class TabOrders : TerminalTab
     [SerializeField]
     private Image _characterIcon;
     [SerializeField]
-    private TMP_Text _description;
+    private TMP_Text _orderDescription;
+    [SerializeField]
+    private TMP_Text _characterDescription;
     [SerializeField]
     private TMP_Text _characterName;
 
@@ -18,7 +20,8 @@ public class TabOrders : TerminalTab
         if (task != null)
         {
             _characterIcon.sprite = task.character.Sprite;
-            _description.text = task.task.Description;
+            _orderDescription.text = task.task.Description;
+            _characterDescription.text = task.character.Description;
             _characterName.text = task.character.Name;
         }
     }
